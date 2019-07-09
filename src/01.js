3538339e6875dc364ef514a1cbcb4f408d7cfdc4
panzerstadt <tliqun@gmail.com>
2019-07-09 12:18:01 +0900

    stuff

---
+++ src/01.js
@@ -0,0 +1,13 @@
+const name2 = "Freddy";
+typeof name2 === "string";
+
+if (!("serviceWorker" in navigator)) {
+  // you have an old browser :-(
+}
+
+const greeting = "hello";
+console.log(`${greeting} world!`);
+[(1, 2, 3)].forEach(x => console.log(x));
+
+// get all your prettier settings easily here
+// https://prettier.io/playground/
